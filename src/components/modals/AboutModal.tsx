8436fbdc2e80893f23eba651383e0583957106a7
Thomas Porteus <1842033+tporteus@users.noreply.github.com>
2022-02-11 19:01:04 +0000

    update

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,13 +9,22 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
+        This is an medical version of the word guessing game we all know and
+        love!
+
+        Built with Love by the team at <a
+          href="https://iatro.health"
+          className="underline font-bold"
+        >
+          @iatro
+        </a>{' '}
+
+        Based on the Open Source {' '}
         <a
           href="https://github.com/cwackerfuss/react-wordle"
           className="underline font-bold"
         >
-          check out the code here
+          React Wordle
         </a>{' '}
       </p>
     </BaseModal>
