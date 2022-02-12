5acc397df6cb492bbd12a469a107c7b60802c85c
Thomas Porteus <1842033+tporteus@users.noreply.github.com>
2022-02-12 11:58:55 +0000

    Update InfoModal.tsx

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -26,33 +26,33 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
+        <Cell value="M" status="correct" />
         <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="D" />
+        <Cell value="I" />
+        <Cell value="C" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter M is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
         <Cell value="O" />
-        <Cell value="T" />
+        <Cell value="L" status="present" />
+        <Cell value="Y" />
+        <Cell value="P" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter L is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
+        <Cell value="M" />
         <Cell value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="C" />
+        <Cell value="U" status="absent" />
+        <Cell value="S" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter U is not in the word in any spot.
